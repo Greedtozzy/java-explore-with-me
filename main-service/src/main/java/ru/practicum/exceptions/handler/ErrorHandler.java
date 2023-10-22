beabc18d44fb7b5f084fe65d680b738db45f05e6
a.arkhipov <arkhipov.antosha@yandex.ru>
2023-10-23 02:21:41 +0300

    Дипломный проект. Часть 2, ревью 1

--- main-service/src/main/java/ru/practicum/exceptions/handler/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exceptions/handler/ErrorHandler.java
@@ -6,44 +6,16 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-//import ru.practicum.categories.controller.AdminCategoryController;
-//import ru.practicum.categories.controller.PublicCategoryController;
-//import ru.practicum.compilations.controller.AdminCompilationController;
-//import ru.practicum.compilations.controller.PublicCompilationController;
-//import ru.practicum.events.controller.AdminEventController;
-//import ru.practicum.events.controller.PublicEventController;
-//import ru.practicum.events.controller.UserEventController;
 import ru.practicum.exceptions.*;
 import ru.practicum.exceptions.dto.ApiError;
-//import ru.practicum.requests.controller.UserRequestController;
-//import ru.practicum.users.controller.AdminUserController;
 import org.hibernate.exception.ConstraintViolationException;
 
-//import javax.persistence.EntityNotFoundException;
 import javax.xml.bind.ValidationException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-//        (assignableTypes = {AdminUserController.class, UserRequestController.class,
-//        AdminEventController.class, PublicEventController.class, UserEventController.class,
-//        AdminCompilationController.class, PublicCompilationController.class,
-//        AdminCategoryController.class, PublicCategoryController.class})
 @Slf4j
 public class ErrorHandler {
-//    @ExceptionHandler({
-//            UserNotFoundException.class,
-//            RequestNotFoundException.class,
-//            LocationNotFoundException.class,
-//            EventNotFoundException.class,
-//            CompilationNotFoundException.class,
-//            CategoryNotFoundException.class
-//    })
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public ApiError handleNotFoundException(final EntityNotFoundException e) {
-//        return new ApiError("NOT_FOUND", "The required object was not found.",
-//                e.getMessage(), LocalDateTime.now());
-//    }
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleUserNotFoundException(final UserNotFoundException e) {
